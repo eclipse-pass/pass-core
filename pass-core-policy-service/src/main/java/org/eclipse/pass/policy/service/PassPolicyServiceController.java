4fcc9504ea5e850f84a7a335515bb0b637835d7d
jrmartino <jrm@jhu.edu>
2023-04-21 17:16:46 -0400

    initial new implemntation of simplified policy service - no tests yet

---
+++ pass-core-policy-service/src/main/java/org/eclipse/pass/policy/service/PassPolicyServiceController.java
@@ -0,0 +1,152 @@
+/*
+ *
+ * Copyright 2023 Johns Hopkins University
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package org.eclipse.pass.policy.service;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Set;
+
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yahoo.elide.RefreshableElide;
+import org.eclipse.pass.object.PassClient;
+import org.eclipse.pass.object.model.Policy;
+import org.eclipse.pass.object.model.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PassPolicyServiceController {
+
+    private static final long serialVersionUID = 1L;
+    private String institutionalPolicyId = System.getenv("INSTITUTIONAL_POLICY_ID");
+    private String institutionalRepositoryId = System.getenv("INSTITUTIONAL_REPOSITORY_ID");
+    private static final Logger LOG = LoggerFactory.getLogger(PassPolicyServiceController.class);
+    private final PolicyService policyService;
+
+    @Autowired
+    private RefreshableElide refreshableElide;
+
+    @Autowired
+    public PassPolicyServiceController(RefreshableElide refreshableElide) throws IOException {
+        this.policyService = new PolicyServiceSimpleImpl(refreshableElide);
+    }
+
+    public PassPolicyServiceController(PolicyService policyService) {
+        this.policyService = policyService;
+    }
+
+    @GetMapping("/policy/policies")
+    public void doGetPolicies(HttpServletRequest request, HttpServletResponse response)
+        throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        LOG.info("Servicing new request......");
+        LOG.debug("Context path: " + request.getContextPath() + "; query string " + request.getQueryString());
+
+        // retrieve submission ID from request
+        String submissionParameter = request.getParameter("submission");
+        Long submissionId = Long.parseLong(submissionParameter);
+        Principal userPrincipal = request.getUserPrincipal();
+
+        // handle empty or invalid request submission error
+        if (submissionId == null) {
+            LOG.error("Submission query parameter missing or invalid");
+            response.setStatus(400);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing or invalid submission parameter: " +
+                                                                   "must be a String representation of a Long");
+
+            return;
+        }
+
+        Set<Policy> policies = policyService.findPoliciesForSubmission(submissionId, userPrincipal);
+
+        JsonArrayBuilder jab = Json.createArrayBuilder();
+        for (Policy policy : policies) {
+            JsonObjectBuilder job = Json.createObjectBuilder();
+            job.add("id", policy.getId().toString());
+            if ( policy.getId().toString().equals(institutionalPolicyId)) {
+                job.add("type", "institution");
+            } else {
+                job.add("type", "funder");
+            }
+            jab.add(job.build());
+        }
+        set_response(response, (JsonObject) jab.build(), HttpStatus.OK);
+    }
+
+    @GetMapping("/policy/repositories")
+    public void doGetRepositories(HttpServletRequest request, HttpServletResponse response)
+        throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        LOG.info("Servicing new request......");
+        LOG.debug("Context path: " + request.getContextPath() + "; query string " + request.getQueryString());
+
+        // retrieve submission parameter value from request
+        String submissionParameterValue = request.getParameter("submission");
+        Long submissionId = Long.parseLong(submissionParameterValue);
+        Principal userPrincipal = request.getUserPrincipal();
+
+        // handle empty or invalid request submission error
+        if (submissionId == null) {
+            set_error_response(response, "Missing or invalid submission parameter: " +
+                                         "must be a String representation of a Long", HttpStatus.BAD_REQUEST);
+            return;
+        }
+        Set<Repository> repositories = policyService.findRepositoriesForSubmission(submissionId, userPrincipal);
+        JsonArrayBuilder jab = Json.createArrayBuilder();
+        for (Repository repository : repositories) {
+            JsonObjectBuilder job = Json.createObjectBuilder();
+            job.add("url", PassClient.getUrl(refreshableElide, repository));
+            if ( repository.getId().toString().equals(institutionalRepositoryId)) {
+                job.add("selected", "true");
+            } else {
+                job.add("selected", "false");
+            }
+            jab.add(job.build());
+        }
+        set_response(response, (JsonObject) jab.build(), HttpStatus.OK);
+    }
+
+
+    private void set_response(HttpServletResponse response, JsonObject obj, HttpStatus status) throws IOException {
+        response.getWriter().print(obj.toString());
+        response.setStatus(status.value());
+    }
+
+    private void set_error_response(HttpServletResponse response, String message,
+                                    HttpStatus status) throws IOException {
+        JsonObject obj = Json.createObjectBuilder().add("message", message).build();
+
+        set_response(response, obj, status);
+        LOG.error(message);
+    }
+
+}
---
+++ pass-core-policy-service/src/main/java/org/eclipse/pass/policy/service/PolicyServiceSimpleImpl.java
@@ -0,0 +1,134 @@
+/*
+ *
+ * Copyright 2023 Johns Hopkins University
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package org.eclipse.pass.policy.service;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+
+import com.yahoo.elide.RefreshableElide;
+import org.eclipse.pass.object.PassClient;
+import org.eclipse.pass.object.PassClientResult;
+import org.eclipse.pass.object.PassClientSelector;
+import org.eclipse.pass.object.RSQL;
+import org.eclipse.pass.object.model.Funder;
+import org.eclipse.pass.object.model.Grant;
+import org.eclipse.pass.object.model.Policy;
+import org.eclipse.pass.object.model.Repository;
+import org.eclipse.pass.object.model.Submission;
+import org.eclipse.pass.object.model.User;
+public class PolicyServiceSimpleImpl implements PolicyService {
+
+    private RefreshableElide refreshableElide;
+    private String institutionalPolicyId = System.getenv("INSTITUTIONAL_POLICY_ID");
+    private String institutionalRepositoryId = System.getenv("INSTITUTIONAL_REPOSITORY_ID");
+
+    public PolicyServiceSimpleImpl (RefreshableElide refreshableElide) {
+        this.refreshableElide = refreshableElide;
+    }
+
+    @Override
+    public Set<Policy> findPoliciesForSubmission(Long submissionId, Principal userPrincipal) {
+        Set<Policy> policies = new HashSet<>();
+        JsonArrayBuilder jab = Json.createArrayBuilder();
+        try (PassClient passClient = PassClient.newInstance(refreshableElide)) {
+            Submission submission = passClient.getObject(Submission.class, submissionId);
+            for (Grant grant : submission.getGrants()) {
+                for (Funder funder : getFunders(grant)) {
+                    policies.add(funder.getPolicy());
+                }
+            }
+
+            //If the user is an affiliate of the institution, add the institutional repository
+            Long policyId = Long.parseLong(institutionalPolicyId);
+            if(isInstitutionalUser(userPrincipal) && policyId != null) {
+                Policy policy = passClient.getObject(Policy.class, policyId);
+                if (policy != null) {
+                    policies.add(policy);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return policies;
+    }
+
+    @Override
+    public Set<Repository> findRepositoriesForSubmission(Long submissionId, Principal userPrincipal ) {
+        Set<Repository> repositories = new HashSet<>();
+        for ( Policy policy : findPoliciesForSubmission(submissionId, userPrincipal)) {
+            repositories.addAll(policy.getRepositories());
+        }
+
+        //If the user is an affiliate of the institution, add the institutional repository
+        if (isInstitutionalUser(userPrincipal)) {
+            try(PassClient passClient = PassClient.newInstance(refreshableElide)) {
+                Long repositoryId = Long.parseLong(institutionalRepositoryId);
+                if(repositoryId != null) {
+                    Repository repository = passClient.getObject(Repository.class, repositoryId);
+                    if (repository != null) {
+                        repositories.add(repository);
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return repositories;
+    }
+
+    private Set<Funder> getFunders(Grant grant) {
+        Set<Funder> funders = new HashSet<>();
+        Funder primaryFunder = grant.getPrimaryFunder();
+        Funder directFunder = grant.getDirectFunder();
+
+        if ( primaryFunder != null) {
+            funders.add(primaryFunder);
+        }
+        if (directFunder != null) {
+            funders.add(directFunder);
+        }
+
+        return funders;
+    }
+
+    private boolean isInstitutionalUser (Principal userPrincipal) {
+        if (userPrincipal == null || userPrincipal.getName() == null) {
+
+            String user_name = userPrincipal.getName();
+
+            try (PassClient client = PassClient.newInstance(refreshableElide)) {
+                PassClientSelector<User> selector = new PassClientSelector<>(User.class);
+                selector.setFilter(RSQL.equals("username", user_name));
+                PassClientResult<User> result = client.selectObjects(selector);
+
+                if (result.getObjects().size() == 1) {
+                    return true;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
