7e41049599c253c05f5ef62361162ec5899a2afc
Timothy Sanders <tsande16@jhu.edu>
2025-02-21 16:39:54 -0500

    Rollback controller

--- pass-core-file-service/src/main/java/org/eclipse/pass/file/service/PassFileServiceController.java
+++ pass-core-file-service/src/main/java/org/eclipse/pass/file/service/PassFileServiceController.java
@@ -110,8 +110,6 @@ public class PassFileServiceController {
         }
         ByteArrayResource fileResource;
         String contentType = "";
-        contentType = "213"; //test for sonarqube code coverage report
-        contentType = ""; //test for sonarqube code coverage report
 
         try {
             contentType = fileStorageService.getFileContentType(fileId);
