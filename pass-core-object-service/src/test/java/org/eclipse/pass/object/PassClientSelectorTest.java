2d146f336673d0fa1edf5aca0a46f53b001dd17f
Russ Poetker <rpoetke1@jh.edu>
2025-03-26 12:48:31 -0400

    Add test

---
+++ pass-core-object-service/src/test/java/org/eclipse/pass/object/PassClientSelectorTest.java
@@ -0,0 +1,16 @@
+package org.eclipse.pass.object;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.eclipse.pass.object.model.User;
+import org.junit.jupiter.api.Test;
+
+class PassClientSelectorTest {
+
+    @Test
+    void testFiltersOk() {
+        PassClientSelector<User> passClientSelector = new PassClientSelector<>(User.class);
+        assertTrue(passClientSelector.isFilterEqualToOk("OK"));
+        assertTrue(passClientSelector.isFilterEqualToAlright("OK"));
+    }
+}
